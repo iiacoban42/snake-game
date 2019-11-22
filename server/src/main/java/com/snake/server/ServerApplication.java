f398e45df15a58ebe35dc9731ac0720e0d166588
Oleksandr Fomin <O.V.Fomin@student.tudelft.nl>
2019-11-22 11:53:12 +0100

    First attempt to make CI work

--- server/src/main/java/com/snake/server/ServerApplication.java
+++ server/src/main/java/com/snake/server/ServerApplication.java
@@ -3,9 +3,16 @@ package com.snake.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class to start the server.
+ */
 @SpringBootApplication
 public class ServerApplication {
 
+	/**
+	 * Method starts the server.
+	 * @param args arguments of main method.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
